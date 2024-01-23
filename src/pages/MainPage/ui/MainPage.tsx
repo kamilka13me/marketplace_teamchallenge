f580c28801b57de0c13f054bc098b49dc62640e3
Yura <ogogan45@gmail.com>
2024-01-23 14:43:36 +0200

    add work with api and useAxios hook

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,29 @@
 import { FC } from 'react';
 
 import BugButton from '@/app/providers/ErrorBoundary/ui/BugButton';
+import { useAxios } from '@/shared/lib/hooks/useAxios';
 
 interface Props {}
 
+interface User {
+  id: string;
+  name: string;
+}
+
 const MainPage: FC<Props> = () => {
-  return <BugButton />;
+  const { data, error, loading } = useAxios<User[]>('/users');
+
+  if (loading || !data) return 'Loading...';
+  if (error) return 'Error!';
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>{item.id}</div>
+      ))}
+      <BugButton />
+    </div>
+  );
 };
 
 export default MainPage;
