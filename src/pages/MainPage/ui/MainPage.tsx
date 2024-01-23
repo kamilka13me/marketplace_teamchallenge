3576376e4107966149161e80aea525570fb8248e
Yura <ogogan45@gmail.com>
2024-01-23 12:21:02 +0200

    Create Mainpage and refactoring main.tsx file

---
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import BugButton from '@/app/providers/ErrorBoundary/ui/BugButton';
+
+interface Props {}
+
+const MainPage: FC<Props> = () => {
+  return <BugButton />;
+};
+
+export default MainPage;
