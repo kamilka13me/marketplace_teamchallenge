401d6e7999affc723a8d91d1aa8b67b71c90455f
Yura <ogogan45@gmail.com>
2024-01-30 13:41:41 +0200

    feat: add login slice

---
+++ frontend/src/enteties/User/model/slice/userSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+});
+
+// Action creators are generated for each case reducer function
+// export const {} = userSlice;
+// export const {} = userSlice;
---
+++ frontend/src/features/userAuth/model/slice/loginSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const loginSlice = createSlice({
+  name: '',
+  initialState,
+  reducers: {},
+});
+
+// Action creators are generated for each case reducer function
+// export const {} = loginSlice;
+// export const {} = loginSlice;
