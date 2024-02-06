e980d8ce033c0e742752e0888cded5513dfe3b00
Reecure <ogogan45@gmail.com>
2024-02-07 00:24:23 +0200

    fix: add colors to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      theme: {
+        colors: {
+          primary: '#003E6B',
+          darkest: '#112A43',
+          dark: '#4D5674',
+          medium: '#838CAB',
+          light: '#D8DDF4',
+          lightest: '#F0F4F8',
+          'error-dark': '#A61B1C',
+          'error-light': '#FFEEEE',
+          'warning-dark': '#C99A2D',
+          'warning-light': '#FFFAEA',
+          'true-dark': '#079A82',
+          'true-light': '#F0FCF9',
+        },
+      },
+    },
   },
   plugins: [],
 };
