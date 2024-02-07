21703a82138df05255aecac02ec60ca439a4275a
olehivanovw <olehivanovw@gmail.com>
2024-02-07 22:00:27 +0200

    refactor: added new login selectors.

--- frontend/src/features/userAuth/model/selectors/loginSelectors.ts
+++ frontend/src/features/userAuth/model/selectors/loginSelectors.ts
@@ -1,4 +1,6 @@
 import { StateSchema } from '@/app/providers/StoreProvider';
 
+export const getLoginEmail = (state: StateSchema) => state.login.email;
+export const getLoginPassword = (state: StateSchema) => state.login.password;
 export const loginIsLoading = (state: StateSchema) => state.login.isLoading;
 export const loginHasError = (state: StateSchema) => state.login.error;
