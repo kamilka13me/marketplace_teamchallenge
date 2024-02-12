52cfb59d7d54fd5986d3ec4370ec59bf534a2a1c
Reecure <69355871+Reecure@users.noreply.github.com>
2024-02-12 21:35:53 +0200

    fix: now tests setup correctly (#46)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,21 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
-  },
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.API_URL': JSON.stringify(env.API_URL),
+    },
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: '@', replacement: '/src' }],
+    },
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+  }
+
 });
