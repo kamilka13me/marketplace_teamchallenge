f136d9e3cd3b70768f5ccf1fa004e0819f35feac
Reecure <ogogan45@gmail.com>
2024-02-17 13:44:45 +0200

    feat: add ui components, clear main page

---
+++ frontend/src/shared/ui/Image/Image.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+interface Props {}
+
+const Image: FC<Props> = (props) => {
+  // eslint-disable-next-line no-empty-pattern
+  const {} = props;
+
+  return <>Image</>;
+};
+
+export default Image;
