5f535774927e2d04ddd8a93ca9addfcda290c4e8
Reecure <ogogan45@gmail.com>
2024-02-17 14:41:53 +0200

    chore: prepare files to development

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,19 +1,13 @@
 import { FC, Suspense, useEffect } from 'react';
 
-import { Link } from 'react-router-dom';
-
 import { AppRouter } from '@/app/providers/router';
-import { RouteConfig } from '@/app/providers/router/config/routeConfig';
-import { getIsInitedAuthData, getUserAuthData, userActions } from '@/enteties/User';
+import { getIsInitedAuthData, userActions } from '@/enteties/User';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
 import { useAppSelector } from '@/shared/lib/hooks/useAppSelector';
-import { VStack } from '@/shared/ui/Stack';
 
 const App: FC = () => {
   const dispatch = useAppDispatch();
   const inited = useAppSelector(getIsInitedAuthData);
-
-  const user = useAppSelector(getUserAuthData);
 
   useEffect(() => {
     dispatch(userActions.initAuthData());
@@ -21,13 +15,7 @@ const App: FC = () => {
 
   return (
     <div>
-      <Suspense fallback="Loading...">
-        <VStack gap="6">
-          <Link to={RouteConfig.main.path}>Main</Link>
-          {user && <Link to={RouteConfig.profile.path}>Profile</Link>}
-        </VStack>
-        {inited && <AppRouter />}
-      </Suspense>
+      <Suspense fallback="Loading...">{inited && <AppRouter />}</Suspense>
     </div>
   );
 };
