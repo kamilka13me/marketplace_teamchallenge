7f3a9b4dafe95b65ba3c336b2a00204c0365b73e
Reecure <ogogan45@gmail.com>
2024-02-17 14:20:37 +0200

    fix: fix styles and add new

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,18 +5,10 @@ export default {
     extend: {
       theme: {
         colors: {
-          primary: '#003E6B',
-          darkest: '#112A43',
-          dark: '#4D5674',
-          medium: '#838CAB',
-          light: '#D8DDF4',
-          lightest: '#F0F4F8',
-          'error-dark': '#A61B1C',
-          'error-light': '#FFEEEE',
-          'warning-dark': '#C99A2D',
-          'warning-light': '#FFFAEA',
-          'true-dark': '#079A82',
-          'true-light': '#F0FCF9',
+          primary: 'rgba(217, 192, 27, 1)',
+          darkest: 'rgba(30, 30, 30, 1)',
+          text: 'rgba(0, 0, 0, 1)',
+          background: '#FFFFFF'
         },
       },
     },
