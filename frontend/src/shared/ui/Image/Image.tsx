ed63ed20e405ad4f9122106adb4e186bf6a8777b
Reecure <ogogan45@gmail.com>
2024-02-17 21:30:06 +0200

    feat: add Image component

--- frontend/src/shared/ui/Image/Image.tsx
+++ frontend/src/shared/ui/Image/Image.tsx
@@ -1,12 +1,35 @@
-import { FC } from 'react';
+import { FC, ImgHTMLAttributes } from 'react';
 
-interface Props {}
+interface Props extends ImgHTMLAttributes<HTMLImageElement> {
+  src: string;
+  alt: string;
+  width?: string;
+  height?: string;
+  objectFit?: 'cover' | 'contain';
+  className?: string;
+}
 
 const Image: FC<Props> = (props) => {
-  // eslint-disable-next-line no-empty-pattern
-  const {} = props;
+  const {
+    src,
+    alt,
+    height,
+    width,
+    objectFit = 'cover',
+    className,
+    ...otherProps
+  } = props;
 
-  return <>Image</>;
+  return (
+    <img
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      className={`${objectFit === 'cover' ? 'object-cover' : 'object-contain'} ${className}`}
+      {...otherProps}
+    />
+  );
 };
 
 export default Image;
