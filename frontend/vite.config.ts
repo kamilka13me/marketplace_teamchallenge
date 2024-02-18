a000d8f148138a8bf321313f15aa4a93d8a1b109
Reecure <ogogan45@gmail.com>
2024-02-18 10:25:12 +0200

    fix: add SVG plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
-import {defineConfig, loadEnv} from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig, loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -9,7 +10,7 @@ export default defineConfig(({ command, mode }) => {
     define: {
       'process.env.API_URL': JSON.stringify(env.API_URL),
     },
-    plugins: [react(), basicSsl()],
+    plugins: [react(), svgr({ include: '**/*.svg?react' }), basicSsl()],
     resolve: {
       alias: [{ find: '@', replacement: '/src' }],
     },
@@ -17,6 +18,5 @@ export default defineConfig(({ command, mode }) => {
       port: 3000,
       host: '0.0.0.0',
     },
-  }
-
+  };
 });
