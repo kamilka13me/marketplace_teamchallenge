7e2de95b661e7f685317d20f96153470a1033608
olehivanovw <olehivanovw@gmail.com>
2024-02-18 23:50:52 +0200

    refactor: added ui-styles.

--- frontend/src/shared/ui/Button/Button.tsx
+++ frontend/src/shared/ui/Button/Button.tsx
@@ -1,19 +1,32 @@
-import { ButtonHTMLAttributes, memo } from 'react';
+import { ButtonHTMLAttributes, memo, ReactNode } from 'react';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: string;
+  children: string | ReactNode;
   onClick?: () => void;
+  empty?: boolean;
+  fill?: boolean;
+  outlined?: boolean;
+  className?: string;
 }
 
+const emptyStyle: string =
+  'px-4 py-2 border-none rounded-lg font-normal text-black hover:bg-[#D9C01B] disabled:opacity-30';
+
+const fillStyle: string =
+  'bg-[#D9C01B] px-4 py-2 border-2 border-[#D9C01B] rounded-lg font-normal text-black hover:bg-transparent disabled:opacity-30';
+
+const outlinedStyle: string =
+  'px-4 py-2 border-2 border-[#D9C01B] rounded-lg font-normal text-black hover:bg-[#D9C01B] disabled:opacity-30';
+
 const Button = memo((props: Props) => {
-  const { children, onClick, ...otherProps } = props;
+  const { children, onClick, empty, fill, outlined, className, ...otherProps } = props;
 
   return (
     <button
       onClick={onClick}
       {...otherProps}
       type="button"
-      className="bg-red-500 px-3 py-1 rounded-md font-bold text-white"
+      className={`${empty ? emptyStyle : ''} ${fill ? fillStyle : ''} ${outlined ? outlinedStyle : ''} ${className}`}
     >
       {children}
     </button>
