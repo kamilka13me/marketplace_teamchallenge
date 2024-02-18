4745e6c451714c60ce4a8e4baeade214f8df3372
olehivanovw <olehivanovw@gmail.com>
2024-02-19 00:02:40 +0200

    refactor: added reusable link component.

--- frontend/src/shared/ui/Link/Link.tsx
+++ frontend/src/shared/ui/Link/Link.tsx
@@ -1,12 +1,47 @@
-import { FC } from 'react';
+import { AnchorHTMLAttributes, FC, ReactNode } from 'react';
 
-interface Props {}
+type TargetType = '_self' | '_blank' | '_parent' | '_top';
+
+type ReferrerType =
+  | 'no-referrer'
+  | 'no-referrer-when-downgrade'
+  | 'origin'
+  | 'origin-when-cross-origin'
+  | 'same-origin'
+  | 'strict-origin'
+  | 'strict-origin-when-cross-origin'
+  | 'unsafe-url';
+
+interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  children: string | ReactNode;
+  href: string;
+  target?: TargetType;
+  referrerPolicy?: ReferrerType;
+  className?: string;
+}
 
 const Link: FC<Props> = (props) => {
   // eslint-disable-next-line no-empty-pattern
-  const {} = props;
+  const {
+    children,
+    href,
+    target = '_blank',
+    referrerPolicy,
+    className,
+    ...otherProps
+  } = props;
 
-  return <>Link</>;
+  return (
+    <a
+      href={href}
+      target={target}
+      referrerPolicy={referrerPolicy}
+      className={className}
+      {...otherProps}
+    >
+      {children}
+    </a>
+  );
 };
 
 export default Link;
