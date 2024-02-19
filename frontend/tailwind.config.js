5e5a907174be8a71ee1c185e9cccc87efec8a208
Reecure <69355871+Reecure@users.noreply.github.com>
2024-02-19 20:18:28 +0200

    Hot fix tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,14 +3,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      theme: {
         colors: {
           primary: 'rgba(217, 192, 27, 1)',
           darkest: 'rgba(30, 30, 30, 1)',
           text: 'rgba(0, 0, 0, 1)',
           background: '#FFFFFF'
         },
-      },
     },
   },
   plugins: [],
