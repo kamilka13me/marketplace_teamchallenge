4e46e6aff0bbbed447e6caa44d7a13a022fe8c4b
olehivanovw <olehivanovw@gmail.com>
2024-02-21 18:13:24 +0200

    fix: changed Link component.

--- frontend/src/shared/ui/Link/Link.tsx
+++ frontend/src/shared/ui/Link/Link.tsx
@@ -1,46 +1,20 @@
-import { AnchorHTMLAttributes, FC, ReactNode } from 'react';
+import { FC, ReactNode } from 'react';
 
-type TargetType = '_self' | '_blank' | '_parent' | '_top';
+import { NavLink, NavLinkProps } from 'react-router-dom';
 
-type ReferrerType =
-  | 'no-referrer'
-  | 'no-referrer-when-downgrade'
-  | 'origin'
-  | 'origin-when-cross-origin'
-  | 'same-origin'
-  | 'strict-origin'
-  | 'strict-origin-when-cross-origin'
-  | 'unsafe-url';
-
-interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
+interface LinkProps extends NavLinkProps {
+  to: string;
   children: string | ReactNode;
-  href: string;
-  target?: TargetType;
-  referrerPolicy?: ReferrerType;
   className?: string;
 }
 
-const Link: FC<Props> = (props) => {
-  // eslint-disable-next-line no-empty-pattern
-  const {
-    children,
-    href,
-    target = '_blank',
-    referrerPolicy,
-    className,
-    ...otherProps
-  } = props;
+const Link: FC<LinkProps> = (props) => {
+  const { to, children, className, ...otherProps } = props;
 
   return (
-    <a
-      href={href}
-      target={target}
-      referrerPolicy={referrerPolicy}
-      className={className}
-      {...otherProps}
-    >
+    <NavLink to={to} className={className} {...otherProps}>
       {children}
-    </a>
+    </NavLink>
   );
 };
 
