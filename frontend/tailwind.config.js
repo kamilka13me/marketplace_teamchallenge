b933a5f475f6c9ffa41e5cac154d78caa99039f3
Reecure <69355871+Reecure@users.noreply.github.com>
2024-02-21 18:24:41 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,12 +3,27 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-        colors: {
-          primary: 'rgba(217, 192, 27, 1)',
-          darkest: 'rgba(30, 30, 30, 1)',
-          text: 'rgba(0, 0, 0, 1)',
-          background: '#FFFFFF'
-        },
+      fontFamily: {
+        'ibm-plex-sans': ['IBM Plex Sans', 'system-ui', 'sans-serif'],
+        outfit: ['Outfit', 'system-ui', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom-base': ' 0px 2px 11.300000190734863px 0px rgba(118, 110, 110, 0.25)',
+      },
+      colors: {
+        primary: '#D9C01B',
+        secondary: '#FFDE00',
+        black: '#000000',
+        green: '#32C42F',
+        gray: '#8D8D8D',
+        orange: '#D75810',
+        red: '#C13940',
+        white: '#FFFFFF',
+        'gray-100': '#FFFFFF77',
+        'gray-200': '#FFFFFF1D',
+        'gray-600': '#FFDE0000',
+        'gray-900': '#1E1E1E',
+      },
     },
   },
   plugins: [],
