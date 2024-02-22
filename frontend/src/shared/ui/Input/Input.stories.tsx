0f020363fe6e37bc813ba7470cc9a741bbf0648b
olehivanovw <olehivanovw@gmail.com>
2024-02-22 13:54:38 +0200

    chore: updated Input story.

--- frontend/src/shared/ui/Input/Input.stories.tsx
+++ frontend/src/shared/ui/Input/Input.stories.tsx
@@ -11,21 +11,107 @@ const meta = {
       session: 'unknown',
     },
   },
+  argTypes: {
+    error: {
+      type: 'string',
+      description: 'Error text at the bottom of the input',
+    },
+    label: {
+      type: 'string',
+      description: 'Label text at the top of the input',
+    },
+    type: {
+      type: 'string',
+      description: 'Input types options',
+      options: [
+        'button',
+        'checkbox',
+        'color',
+        'date',
+        'datetime-local',
+        'email',
+        'file',
+        'hidden',
+        'image',
+        'month',
+        'number',
+        'password',
+        'radio',
+        'range',
+        'reset',
+        'search',
+        'submit',
+        'tel',
+        'text',
+        'time',
+        'url',
+        'week',
+      ],
+      control: { type: 'select' },
+    },
+    placeholder: {
+      type: 'string',
+      description: 'A short hint in the input',
+    },
+    name: {
+      type: 'string',
+      description: 'Name of the input',
+    },
+    value: {
+      type: 'string',
+      description: 'Sets the value of the element',
+    },
+    readonly: {
+      type: 'boolean',
+      description: 'Indicates that the input field is read-only',
+    },
+    onChange: {
+      type: 'function',
+      description: 'Action function for on input',
+    },
+    onBlur: {
+      type: 'function',
+      description: 'Function when the element loses focus',
+    },
+    variant: {
+      type: 'string',
+      description: 'Button styling options',
+      options: ['basic', 'search'],
+      control: { type: 'select' },
+    },
+    className: {
+      type: 'string',
+      description: 'Description of additional input classes',
+    },
+  },
   tags: ['autodocs'],
 } satisfies Meta<typeof Input>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const InputStory: Story = {
+export const InputBasic: Story = {
   args: {
-    error: 'some error',
-    label: 'Test Input',
+    label: 'Some label',
+    error: 'Some error',
     type: 'text',
     name: 'Name',
     value: '',
+    variant: 'basic',
     onChange: () => {},
     placeholder: 'Test input',
   },
-  name: 'Default',
+  name: 'Basic',
+};
+
+export const InputSearch: Story = {
+  args: {
+    type: 'text',
+    name: 'Name',
+    value: '',
+    variant: 'search',
+    onChange: () => {},
+    placeholder: 'Test input',
+  },
+  name: 'Search',
 };
