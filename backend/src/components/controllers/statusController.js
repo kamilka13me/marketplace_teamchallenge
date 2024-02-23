af2fcc791ea3d35b5647c478e964ccba40397558
kamilka13me <kamilka13me@gmail.com>
2024-02-23 23:21:54 +0200

    fix: fix categories parent id error

--- backend/src/components/controllers/statusController.js
+++ backend/src/components/controllers/statusController.js
@@ -11,6 +11,29 @@ const StatusController = {
       serverTime: currentDate,
     });
   },
+
+  clear: async (req, res) => {
+    const { count } = req.params;
+    const numberOfTimes = parseInt(count, 10);
+
+    if (Number.isNaN(numberOfTimes)) {
+      return res.status(400).send('Invalid count provided');
+    }
+
+    for (let i = 0; i < numberOfTimes; i += 1) {
+      /* eslint-disable no-console */
+      console.log('\\');
+    }
+
+    const currentDate = new Date().toLocaleString();
+
+    /* eslint-disable no-console */
+    console.log(`Current date: ${currentDate}`);
+
+    res.send(
+      `Message printed to console ${numberOfTimes} times. Current date: ${currentDate}`,
+    );
+  },
 };
 
 export default StatusController;
--- backend/src/components/routes/statusRoute.js
+++ backend/src/components/routes/statusRoute.js
@@ -43,4 +43,40 @@ router.get('/ping', StatusController.ping);
  */
 router.post('/info', StatusController.info);
 
+/**
+ * @swagger
+ * /status/clear/{count}:
+ *   get:
+ *     summary: Prints backslashes a specified number of times and shows current date
+ *     description: This endpoint prints the backslash symbol (`\`) a specified number of times to the console and returns the current date.
+ *     tags: [Status]
+ *     parameters:
+ *       - in: path
+ *         name: count
+ *         required: true
+ *         description: Number of times to print the backslash symbol
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully printed the specified number of backslashes and returned the current date.
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               example: "Message printed to console 10 times. Current date: 2024-02-20T12:00:00"
+ *       400:
+ *         description: Invalid count provided
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Invalid count provided"
+ */
+
+router.get('/clear/:count', StatusController.clear);
+
 export default router;
