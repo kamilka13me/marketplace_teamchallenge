0660a7314b249fb4746f02e2a0bfb940f3b8cce9
Reecure <ogogan45@gmail.com>
2024-02-23 22:20:17 +0200

    feat: fix api add proxy

--- frontend/src/shared/api/api.ts
+++ frontend/src/shared/api/api.ts
@@ -1,9 +1,5 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 export const $api = axios.create({
   baseURL: process.env.API_URL,
-  headers: {
-    Authorization: `Bearer ${Cookies.get('token')}`,
-  },
 });
