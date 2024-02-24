a4a06432e497cb0d9a5ad997303f76a4965e58d0
Reecure <69355871+Reecure@users.noreply.github.com>
2024-02-24 13:15:54 +0200

    Update rtkApi.ts

--- frontend/src/shared/api/rtkApi.ts
+++ frontend/src/shared/api/rtkApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const rtkApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.API_URL,
+    baseUrl: '/api',
   }),
   endpoints: () => ({}),
 });
