652461c5ada6b0472f027c038fb5cfa6d359e8b4
olehivanovw <olehivanovw@gmail.com>
2024-02-28 00:26:51 +0200

    refactor: updated Login and RegistrationForm

--- frontend/src/features/userAuth/ui/LoginForm/LoginForm.tsx
+++ frontend/src/features/userAuth/ui/LoginForm/LoginForm.tsx
@@ -1,12 +1,15 @@
-import { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { actionReducer } from '@/features/userAuth';
+import privateEye from '@/shared/assets/icons/private-eye.svg?react';
+import unPrivateEye from '@/shared/assets/icons/unprivate-eye.svg?react';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
+import { Icon } from '@/shared/ui/Icon';
 import { Input } from '@/shared/ui/Input';
 import { Link } from '@/shared/ui/Link';
-import { Text } from '@/shared/ui/Text';
+import { VStack } from '@/shared/ui/Stack';
 
 interface Props {}
 
@@ -44,24 +47,24 @@ const LoginForm: FC<Props> = () => {
   };
 
   return (
-    <div>
-      <Text Tag="h3" text="Login" />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          variant="basic"
-          placeholder="Email"
-          type="text"
-          {...register('inputEmail', {
-            required: 'This field is required',
-            minLength: { value: 5, message: 'Your login must be min 6 symbols' },
-            pattern: {
-              value:
-                /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
-              message: 'Your login must be xxxx@xxx.xx templates',
-            },
-          })}
-          error={errors?.inputEmail && errors?.inputEmail.message}
-        />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        variant="basic"
+        placeholder="Email"
+        type="text"
+        {...register('inputEmail', {
+          required: 'This field is required',
+          minLength: { value: 5, message: 'Your login must be min 6 symbols' },
+          pattern: {
+            value:
+              /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
+            message: 'Your login must be xxxx@xxx.xx templates',
+          },
+        })}
+        error={errors?.inputEmail && errors?.inputEmail.message}
+        className="mt-6"
+      />
+      <div className="relative mt-10">
         <Input
           variant="basic"
           placeholder="Password"
@@ -72,31 +75,39 @@ const LoginForm: FC<Props> = () => {
           })}
           error={errors?.inputPassword && errors?.inputPassword.message}
         />
-        <Input
-          variant="basic"
-          name="showPass"
-          onChange={onTogglePassVisibility}
-          type="checkbox"
-          label="Show password"
+        <Icon
+          clickable
+          onClick={onTogglePassVisibility}
+          Svg={passShown ? privateEye : unPrivateEye}
+          width={24}
+          height={24}
+          className="absolute top-[12px] right-[12px]"
         />
-        <Input
-          variant="basic"
-          value="Log In"
-          name="btnInput"
-          type="submit"
-          disabled={!isValid}
-          className="bg-red-500 px-3 py-1 border-none rounded-md font-bold text-white hover:bg-sky-700 disabled:opacity-30"
-        />
-        <p>
-          Forgot
-          <Link to="/"> Password?</Link>
+      </div>
+      <Link to="/">
+        <p className="outfit text-right text-gray-900 text-[14px] font-normal leading-[18px] mt-5 mb-6">
+          Forgot Password?
         </p>
-        <p>
+      </Link>
+      <Input
+        variant="clear"
+        value="Log In"
+        name="btnInput"
+        type="submit"
+        disabled={!isValid}
+        className="outfit bg-primary min-w-full py-[4px] rounded-lg font-normal text-[18px] leading-[40px] text-black duration-300 hover:bg-secondary active:bg-primary disabled:text-white-300 disabled:bg-white-400"
+      />
+      <VStack align="center" className="mt-6" justify="between">
+        <span className="outfit text-right text-gray-900 text-[14px] font-normal leading-[18px]">
           Don`t have an account?
-          <Link to="/"> Sign up</Link>
-        </p>
-      </form>
-    </div>
+        </span>
+        <Link to="/">
+          <span className="outfit text-right text-black text-[14px] font-semibold decoration-solid decoration-black underline decoration-1">
+            Sign up
+          </span>
+        </Link>
+      </VStack>
+    </form>
   );
 };
 
--- frontend/src/features/userAuth/ui/RegistrationForm/RegistrationForm.tsx
+++ frontend/src/features/userAuth/ui/RegistrationForm/RegistrationForm.tsx
@@ -1,19 +1,23 @@
-import { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { SubmitHandler, useForm } from 'react-hook-form';
 
+import privateEye from '@/shared/assets/icons/private-eye.svg?react';
+import unPrivateEye from '@/shared/assets/icons/unprivate-eye.svg?react';
+import { Checkbox } from '@/shared/ui/Checkbox';
+import { Icon } from '@/shared/ui/Icon';
 import { Input } from '@/shared/ui/Input';
 import { Link } from '@/shared/ui/Link';
-import { Text } from '@/shared/ui/Text';
+import { VStack } from '@/shared/ui/Stack';
 
 interface Props {}
 
 interface InputsValues {
   inputName: string;
-  inputSurname: string;
   inputEmail: string;
   inputPhone: string;
-  inputPass: string;
+  inputPassword: string;
+  personalTerms: string;
 }
 
 const RegistrationForm: FC<Props> = () => {
@@ -42,89 +46,83 @@ const RegistrationForm: FC<Props> = () => {
   };
 
   return (
-    <div>
-      <Text Tag="h3" text="Sign up" />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          variant="basic"
-          placeholder="Name"
-          type="text"
-          {...register('inputName', {
-            required: 'This field is required',
-            minLength: { value: 3, message: 'Your name must be min 3 symbols' },
-            maxLength: { value: 15, message: 'Your name must be max 15 symbols' },
-          })}
-          error={errors?.inputName && errors?.inputName.message}
-        />
-        <Input
-          variant="basic"
-          placeholder="Surname"
-          type="text"
-          {...register('inputSurname', {
-            required: 'This field is required',
-            minLength: { value: 3, message: 'Your surname must be min 3 symbols' },
-            maxLength: { value: 20, message: 'Your surname must be max 20 symbols' },
-          })}
-          error={errors?.inputSurname && errors?.inputSurname.message}
-        />
-        <Input
-          variant="basic"
-          placeholder="Email"
-          type="text"
-          {...register('inputEmail', {
-            required: 'This field is required',
-            minLength: { value: 5, message: 'Your login must be min 6 symbols' },
-            pattern: {
-              value:
-                /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
-              message: 'Your login must be xxxx@xxx.xx templates',
-            },
-          })}
-          error={errors?.inputEmail && errors?.inputEmail.message}
-        />
-        <Input
-          variant="basic"
-          placeholder="Phone"
-          type="text"
-          {...register('inputPhone', {
-            pattern: {
-              value: /^\+?3?8?\s?0\d{2}\)?\s?\d{3}[-\s]?\d{2}[-\s]?\d{2}$/,
-              message: 'Your phone must be Ukrainian',
-            },
-          })}
-          error={errors?.inputPhone?.message}
-        />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        variant="basic"
+        placeholder="Name"
+        type="text"
+        {...register('inputName', {
+          required: 'This field is required',
+          minLength: { value: 3, message: 'Your name must be min 3 symbols' },
+          maxLength: { value: 15, message: 'Your name must be max 15 symbols' },
+        })}
+        error={errors?.inputName && errors?.inputName.message}
+        className="mt-6"
+      />
+      <Input
+        variant="basic"
+        placeholder="Email"
+        type="text"
+        {...register('inputEmail', {
+          required: 'This field is required',
+          minLength: { value: 5, message: 'Your login must be min 6 symbols' },
+          pattern: {
+            value:
+              /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
+            message: 'Your login must be xxxx@xxx.xx templates',
+          },
+        })}
+        error={errors?.inputEmail && errors?.inputEmail.message}
+        className="mt-10"
+      />
+      <div className="relative mt-10 mb-10">
         <Input
           variant="basic"
           placeholder="Password"
           type={passShown ? 'text' : 'password'}
-          {...register('inputPass', {
+          {...register('inputPassword', {
             required: 'This field is required',
             minLength: { value: 5, message: 'Your login must be min 6 symbols' },
           })}
-          error={errors?.inputPass && errors?.inputPass.message}
+          error={errors?.inputPassword && errors?.inputPassword.message}
         />
-        <Input
-          variant="basic"
-          name="showPass"
-          onChange={onTogglePassVisibility}
-          type="checkbox"
-          label="Show password"
+        <Icon
+          clickable
+          onClick={onTogglePassVisibility}
+          Svg={passShown ? privateEye : unPrivateEye}
+          width={24}
+          height={24}
+          className="absolute top-[12px] right-[12px]"
         />
-        <Input
-          variant="basic"
-          name="btnInput"
-          type="submit"
-          value="Sign up"
-          disabled={!isValid}
-          className="bg-red-500 px-3 py-1 border-none rounded-md font-bold text-white hover:bg-sky-700 disabled:opacity-30"
-        />
-        <p>
-          Already have an
-          <Link to="/"> account?</Link>
-        </p>
-      </form>
-    </div>
+      </div>
+      <div>RECAPCHA</div>
+      <Checkbox
+        type="checkbox"
+        variant="basic"
+        label="Personal data termins"
+        {...register('personalTerms', {
+          required: true,
+        })}
+      />
+      <Input
+        variant="clear"
+        value="Sign up"
+        name="btnInput"
+        type="submit"
+        disabled={!isValid}
+        className="outfit bg-primary min-w-full py-[4px] mt-6 rounded-lg font-normal text-[18px] leading-[40px] text-black duration-300 hover:bg-secondary active:bg-primary disabled:text-white-300 disabled:bg-white-400"
+      />
+      <VStack align="center" className="mt-6" justify="between">
+        <span className="outfit text-right text-gray-900 text-[14px] font-normal leading-[18px]">
+          Already have an account?
+        </span>
+        <Link to="/">
+          <span className="outfit text-right text-black text-[14px] font-semibold decoration-solid decoration-black underline decoration-1">
+            Sign up
+          </span>
+        </Link>
+      </VStack>
+    </form>
   );
 };
 
