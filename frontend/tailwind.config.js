14023c1d113e85ba90fa88468ff582219c61f62b
Reecure <ogogan45@gmail.com>
2024-03-01 10:43:42 +0200

    fix: fix hover bug on mobile screens

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {
       fontFamily: {
