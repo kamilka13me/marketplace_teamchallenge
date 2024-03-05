3d0d183979adae06829a5d1cc55b28b7eb3d5164
Reecure <ogogan45@gmail.com>
2024-03-05 10:05:49 +0200

    refactor: move Container to layouts

--- frontend/src/shared/layouts/Container/Container.tsx
+++ frontend/src/shared/layouts/Container/Container.tsx
@@ -2,12 +2,13 @@ import { FC, ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
+  className?: string;
 }
 
 const Container: FC<Props> = (props) => {
-  const { children } = props;
+  const { children, className } = props;
 
-  return <div className="max-w-[1440px] px-16 mx-auto">{children}</div>;
+  return <div className={`max-w-[1440px] px-16 mx-auto ${className}`}>{children}</div>;
 };
 
 export default Container;
