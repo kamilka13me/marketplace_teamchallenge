9b2aa233960348342c6a7489b863a9f5cd01d436
kamilka13me <kamilka13me@gmail.com>
2024-03-08 14:17:37 +0200

    feat: add new login statuses

---
+++ backend/docs/statusCodes.js
@@ -0,0 +1,9 @@
+// 200 ok
+// 400 bad req (system)
+// 500 internal server error
+
+// 201 created
+// 404
+// 409 dublicate email in registration
+// 403 incorect password
+// 422 incorect email while login
