89c3d2048bf414933f62894c701b9b2465cfb44a
olehivanovw <olehivanovw@gmail.com>
2024-03-08 22:13:20 +0200

    feat: added logic for create new user.

---
+++ frontend/src/enteties/User/model/services/setNewUser.ts
@@ -0,0 +1,45 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import { $api } from '@/shared/api/api';
+import { ApiRoutes } from '@/shared/const/apiEndpoints';
+
+interface ApiResponse {
+  username: string;
+  surname: string;
+  email: string;
+  password: string;
+  role: string;
+  views: string[];
+  wishlist: string[];
+}
+
+interface newUserDataProps {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export const setNewUser = createAsyncThunk<ApiResponse, newUserDataProps>(
+  'users',
+  async (newUserData, thunkApi) => {
+    const { rejectWithValue } = thunkApi;
+
+    try {
+      const response = await $api.post<ApiResponse>(ApiRoutes.USER, {
+        ...newUserData,
+      });
+
+      if (response.status !== 200) {
+        return rejectWithValue(`:: ${response.statusText} `);
+      }
+
+      return response.data;
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        return rejectWithValue(`:: ${e.message}`);
+      }
+
+      return rejectWithValue('Unknown error occurred');
+    }
+  },
+);
