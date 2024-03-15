e6b9e1c27b542fb878d612910746f2bf575dc1ef
olehivanovw <olehivanovw@gmail.com>
2024-03-15 15:53:53 +0200

    refactor: updated import.

--- frontend/src/enteties/User/index.ts
+++ frontend/src/enteties/User/index.ts
@@ -7,6 +7,8 @@ import { userActions, userReducer } from './model/slice/userSlice';
 import { User, UserSchema, UserWithWishlist } from './model/types/user';
 
 import { getUserWishlist } from '@/enteties/User/model/services/getUserWishlist';
+import { setInformationUser } from '@/enteties/User/model/services/setInformationUser';
+import { setNewUser } from '@/enteties/User/model/services/setNewUser';
 
 export type { User, UserWithWishlist, UserSchema };
 
@@ -17,4 +19,6 @@ export {
   getWishlist,
   getUserAuthData,
   getIsInitedAuthData,
+  setNewUser,
+  setInformationUser,
 };
