bfc7ade8d58f44b21e379d65de35d4befcb084f7
Reecure <69355871+Reecure@users.noreply.github.com>
2024-03-20 21:40:36 +0200

    feat: add blocks rendering to all categories (#145)

--- frontend/src/widgets/ModalCategory/ui/SubCategory.tsx
+++ frontend/src/widgets/ModalCategory/ui/SubCategory.tsx
@@ -14,25 +14,42 @@ interface SubCategoryProps {
 const SubCategory = (props: SubCategoryProps) => {
   const { data, isFirstSubCategory, closeModal } = props;
 
+  const chunkArray = (arr: Category[], size: number) => {
+    const chunks = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+      chunks.push(arr.slice(i, i + size));
+    }
+
+    return chunks;
+  };
+
+  const chunks = chunkArray(data, Math.ceil(data.length / 4));
+
   if (!data) {
     return null;
   }
 
   return (
-    <ul className={`${isFirstSubCategory && 'grid grid-cols-4'} `}>
-      {data.map((item) => (
-        <li key={item._id}>
-          <NavLink
-            to={`${getRouteProducts()}?category=${item._id}`}
-            className={`capitalize truncate ${isFirstSubCategory && 'font-bold '}`}
-            onClick={() => closeModal()}
-          >
-            {item.name}
-          </NavLink>
-          <SubCategory closeModal={closeModal} data={item.subcategories} />
-        </li>
+    <div className={`${isFirstSubCategory && 'grid grid-cols-4'} `}>
+      {chunks.map((chunk, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <ul key={index}>
+          {chunk.map((item) => (
+            <li key={item._id}>
+              <NavLink
+                to={`${getRouteProducts()}?category=${item._id}`}
+                className={`capitalize truncate ${isFirstSubCategory && 'font-bold '}`}
+                onClick={() => closeModal()}
+              >
+                {item.name}
+              </NavLink>
+              <SubCategory closeModal={closeModal} data={item.subcategories} />
+            </li>
+          ))}
+        </ul>
       ))}
-    </ul>
+    </div>
   );
 };
 
