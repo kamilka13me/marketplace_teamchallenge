3301d32be32f067e44065f228900146455e51dbf
olehivanovw <olehivanovw@gmail.com>
2024-03-20 19:45:27 +0200

    chore: moved into new folder.

--- frontend/src/widgets/PersonalDataForms/ui/PersonalDataForms.stories.tsx
+++ frontend/src/widgets/PersonalDataForms/ui/PersonalDataForms.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 
-import { PersonalDataForms } from '@/widgets/PersonalDataForms/index';
+import { PersonalDataForms } from '@/widgets/PersonalDataForms';
 
 const meta = {
   title: 'Widgets/PersonalDataForms',
