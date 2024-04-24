fdbda2683d77706adf202c4d0abaef409fc3c9d5
Yurii <ogogan45@gmail.com>
2024-04-24 17:37:23 +0300

    fix: fix managing feedbacks #2

--- frontend/src/features/managingFeedbacks/helpers/managingFeedbacksHelpers.ts
+++ frontend/src/features/managingFeedbacks/helpers/managingFeedbacksHelpers.ts
@@ -3,32 +3,39 @@ interface NumbersMap {
 }
 
 export const totalRatingCountHelper = (dataForCalc: NumbersMap) => {
-  const totalCountOfRatings = Object.values(dataForCalc).reduce(
-    (acc, cur) => acc + cur,
-    0,
-  );
+  if (dataForCalc !== null && dataForCalc !== undefined) {
+    return Object.values(dataForCalc).reduce((acc, cur) => acc + cur, 0);
+  }
 
-  return totalCountOfRatings;
+  return 0;
 };
 
 export const calcRatingInPercentage = (dataForCalc: NumbersMap) => {
-  const inPercentages: NumbersMap = {};
-  const totalCount = totalRatingCountHelper(dataForCalc);
+  if (dataForCalc !== null && dataForCalc !== undefined) {
+    const inPercentages: NumbersMap = {};
+    const totalCount = totalRatingCountHelper(dataForCalc);
 
-  Object.keys(dataForCalc).forEach((key) => {
-    inPercentages[key] = ((dataForCalc[key] || 0) / totalCount) * 100;
-  });
+    Object.keys(dataForCalc).forEach((key) => {
+      inPercentages[key] = ((dataForCalc[key] || 0) / totalCount) * 100;
+    });
 
-  return inPercentages;
+    return inPercentages;
+  }
+
+  return 0;
 };
 
 export const calcAverage = (dataForCalc: NumbersMap) => {
-  let sum = 0;
-  const totalCount = totalRatingCountHelper(dataForCalc);
+  if (dataForCalc !== null && dataForCalc !== undefined) {
+    let sum = 0;
+    const totalCount = totalRatingCountHelper(dataForCalc);
 
-  Object.keys(dataForCalc).forEach((key) => {
-    sum += parseInt(key, 10) * (dataForCalc[key] || 0);
-  });
+    Object.keys(dataForCalc).forEach((key) => {
+      sum += parseInt(key, 10) * (dataForCalc[key] || 0);
+    });
 
-  return Number.isNaN(sum / totalCount) ? 0 : sum / totalCount;
+    return Number.isNaN(sum / totalCount) ? 0 : sum / totalCount;
+  }
+
+  return 0;
 };
