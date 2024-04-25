b2952cf7dec2e2c91465a4c40b32395478a2419f
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-04-25 12:24:44 +0300

    feat: add EmailSuccessfully component (#208)

---
+++ frontend/src/widgets/EmailSuccessfully/ui/EmailSuccessfully.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+
+import { getRouteProfile } from '@/shared/const/routes';
+import { Button } from '@/shared/ui/Button';
+
+const EmailSuccessfully = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex justify-center pt-[100px] pb-[180px]">
+      <div className="w-[343px] md:w-[360px]">
+        <h1 className="flex justify-center text-[24px] md:text-[32px] mb-[32px] md:mb-[40px]">
+          Вітаємо!
+        </h1>
+        <p className="flex justify-center text-[16px] text-main-dark mb-[20px] md:text-[18px] ">
+          Ви успішно підтвердили свій профіль.
+        </p>
+        <Button
+          variant="gradient"
+          className=" py-4 w-full self-center md:self-start"
+          onClick={() => {
+            navigate(getRouteProfile('info'));
+          }}
+        >
+          Перейти в кабінет
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default EmailSuccessfully;
