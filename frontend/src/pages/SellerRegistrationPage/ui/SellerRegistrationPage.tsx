40be6ae57995d72d7b14178a6ab65702e0f911e9
olehivanovw <olehivanovw@gmail.com>
2024-05-02 22:55:47 +0300

    refactor: update page.

--- frontend/src/pages/SellerRegistrationPage/ui/SellerRegistrationPage.tsx
+++ frontend/src/pages/SellerRegistrationPage/ui/SellerRegistrationPage.tsx
@@ -1,14 +1,12 @@
 import { FC } from 'react';
 
+import { SellerRegistrationForm } from '@/features/userAuth/ui/SellerRegistrationForm';
 import { Container } from '@/shared/layouts/Container';
-import { HStack } from '@/shared/ui/Stack';
 
 const SellerRegistrationPage: FC = () => {
   return (
     <Container className="h-full">
-      <HStack align="center" justify="center" className="h-full py-[60px] md:py-[140px]">
-        <div>Seller Registration Page</div>
-      </HStack>
+      <SellerRegistrationForm />
     </Container>
   );
 };
