27ac79762605c33946af0978038732d3bb76b800
kamilka13me <125221637+kamilka13me@users.noreply.github.com>
2024-05-04 10:35:37 +0300

    fix: fix seller (#226)

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -87,6 +87,10 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  isAccountActive: {
+    type: Boolean,
+    default: true,
+  },
   views: [
     {
       type: mongoose.Schema.Types.ObjectId,
