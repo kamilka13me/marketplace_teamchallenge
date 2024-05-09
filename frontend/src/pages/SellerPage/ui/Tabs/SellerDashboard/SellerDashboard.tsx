c50b3888ae7d14b792a0ac69d33a5ba5e560eaa9
Yurii <ogogan45@gmail.com>
2024-05-09 21:26:52 +0300

    feat: add mobile dashboard

--- frontend/src/pages/SellerPage/ui/Tabs/SellerDashboard/SellerDashboard.tsx
+++ frontend/src/pages/SellerPage/ui/Tabs/SellerDashboard/SellerDashboard.tsx
@@ -4,7 +4,6 @@ import { getUserAuthData } from '@/enteties/User';
 import CallsClickChart from '@/pages/SellerPage/ui/Tabs/SellerDashboard/CallsClickChart';
 import ContactOpeningChart from '@/pages/SellerPage/ui/Tabs/SellerDashboard/ContactOpeningChart';
 import { useAppSelector } from '@/shared/lib/hooks/useAppSelector';
-import { HStack } from '@/shared/ui/Stack';
 import { EmailConfirmation } from '@/widgets/EmailConfirmation';
 import { QuantityStats } from '@/widgets/QuantityStats';
 
@@ -14,13 +13,11 @@ const SellerDashboard: FC = () => {
   const isAccountConfirmed = user && user.isAccountConfirm;
 
   return (
-    <HStack>
+    <>
       {!isAccountConfirmed && <EmailConfirmation />}
-      <section className="w-full grid gap-4 [grid-template-areas:'views_clicks_calls''contact_contact_calls']">
-        <div className="[grid-area:views]">
+      <section className="w-full grid gap-12 lg:gap-5 mb-12 lg:mb-0 [grid-template-areas:'calls''contact''quantity'] lg:[grid-template-areas:'quantity_quantity_calls''contact_contact_calls']">
+        <div className="[grid-area:quantity] flex w-full justify-between gap-5">
           <QuantityStats stats="views" />
-        </div>
-        <div className="[grid-area:clicks]">
           <QuantityStats stats="clicks" />
         </div>
         <div className="[grid-area:contact]">
@@ -30,7 +27,7 @@ const SellerDashboard: FC = () => {
           <CallsClickChart />
         </div>
       </section>
-    </HStack>
+    </>
   );
 };
 
