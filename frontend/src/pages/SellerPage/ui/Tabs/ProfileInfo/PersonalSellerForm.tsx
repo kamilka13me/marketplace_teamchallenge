744b0cd021caaf0a4e3e87a630ca821ea721812e
olehivanovw <olehivanovw@gmail.com>
2024-05-11 01:12:32 +0300

    feat: added seller form;

---
+++ frontend/src/pages/SellerPage/ui/Tabs/ProfileInfo/PersonalSellerForm.tsx
@@ -0,0 +1,187 @@
+import React, { FC, useState } from 'react';
+
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+
+import { sellerInfoActions } from '@/enteties/Seller/model/slice/sellerSlice';
+import { Seller } from '@/enteties/Seller/model/types/seller';
+import ChangePassBlockSellerForm from '@/pages/SellerPage/ui/Tabs/ProfileInfo/blocks/ChangePassBlockSellerForm';
+import ContactBlockPersonalForm from '@/pages/SellerPage/ui/Tabs/ProfileInfo/blocks/ContactBlockPersonalForm';
+import GeneralBlockPersonalForm from '@/pages/SellerPage/ui/Tabs/ProfileInfo/blocks/GeneralBlockPersonalForm';
+import cancel from '@/shared/assets/icons/cancel.svg?react';
+import unPrivateEye from '@/shared/assets/icons/unprivate-eye.svg?react';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
+import { Button } from '@/shared/ui/Button';
+import { Icon } from '@/shared/ui/Icon';
+import { Input } from '@/shared/ui/Input';
+import { ModalWindow } from '@/shared/ui/ModalWindow';
+import { HStack, VStack } from '@/shared/ui/Stack';
+import { Text } from '@/shared/ui/Text';
+
+const PersonalSellerForm: FC = () => {
+  const { t } = useTranslation();
+
+  const [showChangePass, setShowChangePass] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const dispatch = useAppDispatch();
+
+  const methods = useForm<Seller>({
+    mode: 'all',
+    defaultValues: {
+      email: 'alma.lawson@example.com',
+      password: 'HardcorePassword',
+      contacts: [{ phone: '+38 (050) 550 50 50', person: 'Іванов О.С.' }],
+      communication: [{ messenger: 'Telegram', phone: '+38 (050) 550 50 50' }],
+      descriptCompany: '',
+      generalName: 'Авокадо',
+      generalCommunication: [{ messenger: 'Viber', phone: '+38 (055) 555 55 66' }],
+    },
+  });
+
+  const {
+    handleSubmit,
+    register,
+    formState: { isValid },
+    reset,
+  } = methods;
+
+  const onSubmit: SubmitHandler<Seller> = () => {
+    // console.log(data);
+    reset();
+    setShowModal(!showModal);
+  };
+
+  const onHandlePassForm = () => {
+    setShowChangePass(!showChangePass);
+    dispatch(sellerInfoActions.resetError());
+  };
+
+  const renderRegistrationData = () => (
+    <HStack className="w-full bg-dark-grey rounded-2xl px-4 py-6">
+      <Text
+        Tag="p"
+        text={t('Ваші дані для входу в Маркетплейс')}
+        size="xl"
+        className="leading-[26px] text-main-white mb-5"
+      />
+      <HStack align="start" gap="5" className="w-full lg:flex-row mb-2">
+        <HStack gap="2" className="w-full">
+          <Text Tag="p" text="Email" size="md" color="white" />
+          <Input
+            variant="fill"
+            placeholder="Email"
+            type="text"
+            autoComplete="off"
+            className="min-h-[48px] w-full"
+            classNameBlockWrap="w-full"
+            {...register('email', {
+              disabled: true,
+            })}
+          />
+        </HStack>
+
+        <HStack gap="2" className="w-full">
+          <Text Tag="p" text={t('Пароль')} size="md" color="white" />
+          <div className="relative w-full">
+            <Input
+              variant="fill"
+              placeholder={t('Ваш пароль')}
+              type="password"
+              autoComplete="off"
+              className="min-h-[48px] w-full"
+              {...register('password', {
+                disabled: true,
+              })}
+            />
+            <Icon
+              Svg={unPrivateEye}
+              width={24}
+              height={24}
+              className="absolute top-[12px] right-[12px] fill-main-white"
+            />
+          </div>
+        </HStack>
+      </HStack>
+      <VStack align="center" justify="end" className="w-full">
+        <Button
+          variant="clear"
+          className="outfit text-main text-[14px] leading-[18px] font-normal border-b-main border-b-[1px] disabled:text-disabled disabled:border-b-disabled"
+          onClick={() => {
+            setShowChangePass(!showChangePass);
+          }}
+        >
+          {t('Змінити пароль')}
+        </Button>
+      </VStack>
+    </HStack>
+  );
+
+  return (
+    <div className="w-full">
+      <FormProvider {...methods}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col justify-start gap-5"
+        >
+          {renderRegistrationData()}
+
+          <ContactBlockPersonalForm />
+          <GeneralBlockPersonalForm />
+
+          <Input
+            variant="clear"
+            value={t('Зберегти зміни')}
+            name="btnInput"
+            type="submit"
+            disabled={!isValid}
+            classNameBlockWrap="items-end"
+            className="outfit bg-main w-full lg:max-w-[360px] py-[4px] rounded-lg font-normal text-[18px] leading-[40px] text-main-dark duration-300 hover:bg-secondary-yellow active:bg-main disabled:text-main-white disabled:bg-disabled"
+          />
+        </form>
+      </FormProvider>
+
+      {showChangePass && (
+        <ModalWindow
+          onCloseFunc={onHandlePassForm}
+          className="min-w-[299px] md:w-[424px] bg-selected-dark p-6 md:px-8 md:py-10 rounded-2xl animate-open-forms-modal"
+        >
+          <ChangePassBlockSellerForm onClose={onHandlePassForm} />
+        </ModalWindow>
+      )}
+
+      {showModal && (
+        <ModalWindow
+          onCloseFunc={() => {
+            setShowModal(!showModal);
+          }}
+          className="min-w-[233px] bg-selected-dark px-3 py-4 rounded-2xl animate-open-forms-modal"
+        >
+          <VStack align="center" justify="end">
+            <Icon
+              clickable
+              onClick={() => {
+                setShowModal(!showModal);
+              }}
+              Svg={cancel}
+              width={24}
+              height={24}
+              className="fill-main-white hover:transition hover:rotate-90 hover:duration-300 duration-300"
+            />
+          </VStack>
+          <HStack align="center" className="mt-5 mx-3.5 mb-8 gap-3">
+            <Text Tag="p" text="Вітаємо!" size="md" className="text-main-white" />
+            <Text
+              Tag="p"
+              text={t('Дані успішно змінено')}
+              size="sm"
+              align="center"
+              className="text-main-white"
+            />
+          </HStack>
+        </ModalWindow>
+      )}
+    </div>
+  );
+};
+
+export default PersonalSellerForm;
---
+++ frontend/src/pages/SellerPage/ui/Tabs/ProfileInfo/blocks/GeneralBlockPersonalForm.tsx
@@ -0,0 +1,206 @@
+import React, { FC, Fragment } from 'react';
+
+import { Listbox, Transition } from '@headlessui/react';
+import { Controller, useFieldArray, useFormContext } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { PhoneInput } from 'react-international-phone';
+
+import { Seller } from '@/enteties/Seller/model/types/seller';
+import arrow from '@/shared/assets/icons/arrow-right.svg?react';
+import trash from '@/shared/assets/icons/trash.svg?react';
+import { Button } from '@/shared/ui/Button';
+import { Icon } from '@/shared/ui/Icon';
+import { Input } from '@/shared/ui/Input';
+import { HStack, VStack } from '@/shared/ui/Stack';
+import { Text } from '@/shared/ui/Text';
+
+const GeneralBlockPersonalForm: FC = () => {
+  const { t } = useTranslation();
+
+  const {
+    register,
+    control,
+    formState: { errors },
+  } = useFormContext<Seller>();
+
+  const {
+    fields: generalCommunFields,
+    remove: removeGeneralMethods,
+    append: appendGeneralMethods,
+  } = useFieldArray({
+    control,
+    name: `generalCommunication`,
+  });
+
+  const messengersList: string[] = ['Telegram', 'Viber', 'WhatsUp'];
+
+  const appendGenMethods = () => {
+    appendGeneralMethods({ messenger: '', phone: '' });
+  };
+
+  const removeGenMethods = (indexToRemove: number) => {
+    removeGeneralMethods(indexToRemove);
+  };
+
+  const renderCommunicationMethods = () => (
+    <HStack gap="5" className="w-full">
+      <Text
+        Tag="p"
+        text={t("Контакти для зв'язку з клієнтами")}
+        size="xl"
+        className="leading-[26px] text-main-white"
+      />
+      <HStack gap="5" className="w-full">
+        {generalCommunFields.map((field, index) => (
+          <HStack key={field.id} gap="2" className="w-full">
+            <HStack align="start" gap="4" className="w-full sm:flex-row">
+              {/* MESSENGER LIST */}
+              <div className="relative w-full">
+                <Controller
+                  name={`generalCommunication.${index}.messenger`}
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: false }}
+                  render={({ field }) => (
+                    <Listbox value={field.value} onChange={field.onChange}>
+                      {({ open }) => (
+                        <>
+                          <Listbox.Button
+                            className={`relative w-full ${open ? 'rounded-t-lg' : 'rounded-lg'}  bg-selected-dark px-4 py-3`}
+                          >
+                            <VStack align="center" gap="4">
+                              <Text
+                                className="!text-disabled"
+                                Tag="p"
+                                text={
+                                  field.value === '' ? t('Обрати зі списку') : field.value
+                                }
+                                size="sm"
+                              />
+                              <Icon
+                                Svg={arrow}
+                                className={`ml-auto fill-disabled ${open ? '-rotate-90' : 'rotate-90'}`}
+                                width={24}
+                                height={24}
+                              />
+                            </VStack>
+                          </Listbox.Button>
+                          <Transition
+                            show={open}
+                            as={Fragment}
+                            enter="transition ease-in duration-100"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                          >
+                            <Listbox.Options
+                              className="absolute h-auto w-full overflow-auto rounded-b-lg bg-selected-dark z-40"
+                              static
+                            >
+                              {messengersList.map((value, index) => (
+                                <Listbox.Option
+                                  key={index}
+                                  value={value}
+                                  className="relative flex px-4 py-3 outfit text-[14px] text-disabled font-normal leading-[24px] rounded-lg cursor-pointer hover:text-selected-dark hover:bg-main focus:text-selected-dark focus:bg-main"
+                                >
+                                  {value}
+                                </Listbox.Option>
+                              ))}
+                            </Listbox.Options>
+                          </Transition>
+                        </>
+                      )}
+                    </Listbox>
+                  )}
+                />
+              </div>
+
+              {/* PHONE NUMBER */}
+              <HStack gap="1" className="w-full">
+                <Controller
+                  name={`generalCommunication.${index}.phone`}
+                  control={control}
+                  rules={{
+                    required: t("Це поле є обов'язковим"),
+                  }}
+                  render={({ field }) => (
+                    <VStack align="center" gap="4" justify="start" className="w-full">
+                      <PhoneInput
+                        defaultCountry="ua"
+                        defaultMask=".........."
+                        placeholder={t('Номер телефону')}
+                        value={field.value}
+                        onChange={field.onChange}
+                        onBlur={field.onBlur}
+                        name={field.name}
+                        className="border-none w-full"
+                        inputClassName="!outfit !w-full !min-h-[48px] !min-w-[100px] !pl-4 !bg-selected-dark !placeholder:text-disabled !text-[14px] !text-disabled !font-normal !border-none !rounded-lg !focus:text-disabled !outline-none"
+                        countrySelectorStyleProps={{
+                          className: '!hidden',
+                          buttonClassName: '!bg-transparent !min-h-[48px] !border-none',
+                        }}
+                      />
+                      <Button
+                        disabled={generalCommunFields.length === 1}
+                        variant="clear"
+                        className="stroke-main disabled:stroke-disabled"
+                        onClick={() => removeGenMethods(index)}
+                      >
+                        <Icon Svg={trash} width={24} height={24} />
+                      </Button>
+                    </VStack>
+                  )}
+                />
+                {errors?.generalCommunication?.[index]?.phone && (
+                  <p className="outfit font-normal text-[12px] text-error-red">
+                    {errors?.generalCommunication?.[index]?.phone?.message}
+                  </p>
+                )}
+              </HStack>
+            </HStack>
+            <VStack align="center" justify="start" className="w-full">
+              <Button
+                disabled={generalCommunFields.length === 3}
+                variant="clear"
+                className="outfit text-main text-[14px] leading-[18px] font-normal border-b-main border-b-[1px] disabled:text-disabled disabled:border-b-disabled"
+                onClick={appendGenMethods}
+              >
+                {t('Додати')}
+              </Button>
+            </VStack>
+          </HStack>
+        ))}
+      </HStack>
+    </HStack>
+  );
+
+  return (
+    <HStack justify="start" className="w-full bg-dark-grey rounded-2xl px-4 py-6 gap-5">
+      <Text
+        Tag="p"
+        text={t('Інформація про компанію, доступна на сайті для всіх користувачів')}
+        bold
+        size="2xl"
+        className="leading-[30px] text-main-white"
+      />
+
+      <HStack gap="2" className="w-full">
+        <Text Tag="p" text={t('Назва компанії')} size="md" color="white" />
+        <Input
+          variant="fill"
+          placeholder={t('Введіть назву компанії')}
+          type="text"
+          autoComplete="off"
+          className="min-h-[48px] w-full"
+          classNameBlockWrap="w-full"
+          {...register('generalName', {
+            disabled: true,
+          })}
+        />
+      </HStack>
+
+      {renderCommunicationMethods()}
+    </HStack>
+  );
+};
+
+export default GeneralBlockPersonalForm;
