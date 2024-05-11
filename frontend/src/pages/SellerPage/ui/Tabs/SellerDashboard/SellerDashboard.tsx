7392dc9af938038599290be63e1c39f27882dfdd
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-05-11 13:39:50 +0300

    fix: fix SellerDashboard

--- frontend/src/pages/SellerPage/ui/Tabs/SellerDashboard/SellerDashboard.tsx
+++ frontend/src/pages/SellerPage/ui/Tabs/SellerDashboard/SellerDashboard.tsx
@@ -4,6 +4,7 @@ import { getUserAuthData } from '@/enteties/User';
 import CallsClickChart from '@/pages/SellerPage/ui/Tabs/SellerDashboard/CallsClickChart';
 import ContactOpeningChart from '@/pages/SellerPage/ui/Tabs/SellerDashboard/ContactOpeningChart';
 import { useAppSelector } from '@/shared/lib/hooks/useAppSelector';
+import { HStack } from '@/shared/ui/Stack';
 import { EmailConfirmation } from '@/widgets/EmailConfirmation';
 import { QuantityStats } from '@/widgets/QuantityStats';
 
@@ -13,7 +14,7 @@ const SellerDashboard: FC = () => {
   const isAccountConfirmed = user && user.isAccountConfirm;
 
   return (
-    <>
+    <HStack>
       {!isAccountConfirmed && <EmailConfirmation />}
       <section className="w-full grid gap-12 lg:gap-5 mb-12 lg:mb-0 [grid-template-areas:'calls''contact''quantity'] lg:[grid-template-areas:'quantity_quantity_calls''contact_contact_calls']">
         <div className="[grid-area:quantity] flex w-full justify-between gap-5">
@@ -27,7 +28,7 @@ const SellerDashboard: FC = () => {
           <CallsClickChart />
         </div>
       </section>
-    </>
+    </HStack>
   );
 };
 
