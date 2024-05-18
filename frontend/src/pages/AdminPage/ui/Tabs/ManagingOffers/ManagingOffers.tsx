5f5b21c48edd4fb273e2ebd189d4b0e458e30d60
Redddeve <popko.dmitry797@gmail.com>
2024-05-18 16:46:47 +0300

    chore: styled listings list

--- frontend/src/pages/AdminPage/ui/Tabs/ManagingOffers/ManagingOffers.tsx
+++ frontend/src/pages/AdminPage/ui/Tabs/ManagingOffers/ManagingOffers.tsx
@@ -1,9 +1,19 @@
 import { FC } from 'react';
 
+import OfferController from './OfferController';
+
 import { HStack, VStack } from '@/shared/ui/Stack';
 import { Text } from '@/shared/ui/Text';
 
 const ManagingOffers: FC = () => {
+  const editHandler = () => {};
+
+  const publishHandler = () => {};
+
+  const rejectHandler = () => {};
+
+  const blockHandler = () => {};
+
   return (
     <section className="flex flex-col gap-4 w-full">
       <ul className="bg-dark-grey flex justify-between py-2 px-4 rounded-2xl">
@@ -21,51 +31,112 @@ const ManagingOffers: FC = () => {
         </li>
       </ul>
       <HStack gap="4" className="p-4 bg-dark-grey rounded-2xl ">
-        <VStack className="">
+        <VStack
+          className="w-full
+        "
+        >
           <Text Tag="span" text="Сортування за" size="md" color="white" />
         </VStack>
-        <table>
-          <thead>
+        <table className="w-full border-separate border-spacing-0 ">
+          <thead className="mb-1 bg-selected-dark rounded-2xl">
             <tr>
-              <th aria-label="Дата">
+              <th aria-label="Дата" className="w-[125px] p-2.5 font-normal text-start">
                 <Text Tag="span" text="Дата" size="lg" color="white" />
               </th>
-              <th aria-label="ID Продавця">
+              <th
+                aria-label="ID Продавця"
+                className="w-[140px] p-2.5 font-normal text-start"
+              >
                 <Text Tag="span" text="ID Продавця" size="lg" color="white" />
               </th>
-              <th aria-label="Назва товару">
-                <Text Tag="span" text="Назва товару" size="lg" color="white" />
+              <th
+                aria-label="Назва товару"
+                className="w-[259px] font-normal p-2.5 text-start"
+              >
+                <Text
+                  Tag="span"
+                  text="Назва товару"
+                  size="lg"
+                  color="white"
+                  className="max-w-[220px] max-h-12 overflow-hidden whitespace-nowrap overflow-ellipsis "
+                />
               </th>
-              <th aria-label="ID Товару">
+              <th
+                aria-label="ID Товару"
+                className="w-[120px] font-normal p-2.5 text-start"
+              >
                 <Text Tag="span" text="ID Товару" size="lg" color="white" />
               </th>
-              <th aria-label="Ціна">
+              <th aria-label="Ціна" className="w-[140px] font-normal p-2.5 text-start">
                 <Text Tag="span" text="Ціна" size="lg" color="white" />
               </th>
-              <th aria-label="Дії">
+              <th aria-label="Дії" className="w-[70px] font-normal p-2.5 text-start">
                 <Text Tag="span" text="Дії" size="lg" color="white" />
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td aria-label="Дата">
-                <Text Tag="p" text="placeholder" size="md" color="white" />
+            {/* {offers.map((offer) => (
+              <tr className="h-[76px] mb-1 even:bg-selected-dark odd:bg-transparent rounded-2xl">
+                <td aria-label="Дата" className="pl-2.5 rounded-l-2xl">
+                  <Text Tag="p" text={offer.date} size="md" color="white" />
+                </td>
+                <td aria-label="ID Продавця" className="pl-2.5">
+                  <Text Tag="p" text={offer.sellerId} size="md" color="white" />
+                </td>
+                <td aria-label="Назва товару" className="pl-2.5">
+                  <Text
+                    Tag="p"
+                    text={offer.productName}
+                    size="md"
+                    color="white"
+                  />
+                </td>
+                <td aria-label="ID Товару" className="pl-2.5">
+                  <Text Tag="p" text={offer.productId} size="md" color="white" />
+                </td>
+                <td aria-label="Ціна" className="pl-2.5">
+                  <Text Tag="p" text={offer.productPrice} size="md" color="white" />
+                </td>
+                <td aria-label="Дії" className="rounded-r-2xl">
+                  <Icon
+                    Svg={action}
+                    width={40}
+                    height={59}
+                    className="rotate-90 cursor-pointer"
+                    //   onClick={() => }
+                  />
+                </td>
+              </tr>
+            ))} */}
+            <tr className="h-[76px] mb-1 even:bg-selected-dark odd:bg-transparent rounded-2xl">
+              <td aria-label="Дата" className="pl-2.5 rounded-l-2xl">
+                <Text Tag="p" text="25.04.2024" size="md" color="white" />
               </td>
-              <td aria-label="ID Продавця">
-                <Text Tag="p" text="placeholder" size="md" color="white" />
+              <td aria-label="ID Продавця" className="pl-2.5">
+                <Text Tag="p" text="395151287888" size="md" color="white" />
               </td>
-              <td aria-label="Назва товару">
-                <Text Tag="p" text="placeholder" size="md" color="white" />
+              <td aria-label="Назва товару" className="pl-2.5">
+                <Text
+                  Tag="p"
+                  text="Ноутбук Apple MacBook Air M1 8/256GB 2022"
+                  size="md"
+                  color="white"
+                />
               </td>
-              <td aria-label="ID Товару">
-                <Text Tag="p" text="placeholder" size="md" color="white" />
+              <td aria-label="ID Товару" className="pl-2.5">
+                <Text Tag="p" text="39519889" size="md" color="white" />
               </td>
-              <td aria-label="Ціна">
-                <Text Tag="p" text="placeholder" size="md" color="white" />
+              <td aria-label="Ціна" className="pl-2.5">
+                <Text Tag="p" text="46 228 грн" size="md" color="white" />
               </td>
-              <td aria-label="Дії">
-                <Text Tag="p" text="placeholder" size="md" color="white" />
+              <td aria-label="Дії" className="rounded-r-2xl">
+                <OfferController
+                  editHandler={editHandler}
+                  publishHandler={publishHandler}
+                  rejectHandler={rejectHandler}
+                  blockHandler={blockHandler}
+                />
               </td>
             </tr>
           </tbody>
---
+++ frontend/src/pages/AdminPage/ui/Tabs/ManagingOffers/OfferController.tsx
@@ -0,0 +1,130 @@
+import { FC, useEffect, useState } from 'react';
+
+import publish from '@/shared/assets/icons/approve.svg?react';
+import block from '@/shared/assets/icons/block.svg?react';
+import edit from '@/shared/assets/icons/edit-3.svg?react';
+import action from '@/shared/assets/icons/edit.svg?react';
+import reject from '@/shared/assets/icons/reject.svg?react';
+import { Button } from '@/shared/ui/Button';
+import { Icon } from '@/shared/ui/Icon';
+import { HStack, VStack } from '@/shared/ui/Stack';
+import { Text } from '@/shared/ui/Text';
+
+interface Props {
+  editHandler: () => void;
+  publishHandler: () => void;
+  rejectHandler: () => void;
+  blockHandler: () => void;
+}
+
+const OfferController: FC<Props> = (props) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const { editHandler, publishHandler, rejectHandler, blockHandler } = props;
+
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        setShowModal(false);
+      }
+    };
+
+    if (showModal) {
+      document.addEventListener('keydown', onKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, [showModal]);
+
+  return (
+    <VStack gap="2" className="relative">
+      <Icon
+        Svg={action}
+        width={40}
+        height={59}
+        onClick={() => setShowModal(!showModal)}
+        className="rotate-90 cursor-pointer"
+      />
+      <HStack
+        justify="center"
+        className={`${showModal ? '' : 'hidden'} py-3 px-2 bg-shadow-footer absolute top-[42px] right-0 z-10 rounded-lg`}
+      >
+        <Button
+          variant="clear"
+          className="group flex items-center gap-2 w-[180px] p-2.5 hover:bg-secondary-yellow rounded-lg"
+          onClick={editHandler}
+        >
+          <Icon
+            Svg={edit}
+            width={30}
+            height={30}
+            className="stroke-disabled group-hover:stroke-main-dark"
+          />
+          <Text
+            Tag="span"
+            text="Редагувати"
+            size="md"
+            color="gray-light"
+            className="group-hover:text-main-dark select-none "
+          />
+        </Button>
+        <Button
+          variant="clear"
+          className="group flex items-center gap-2 w-[180px] p-2.5 hover:bg-secondary-yellow rounded-lg"
+          onClick={() => publishHandler()}
+        >
+          <Icon
+            Svg={publish}
+            width={30}
+            height={30}
+            className="stroke-disabled group-hover:stroke-main-dark"
+          />
+          <Text
+            Tag="span"
+            text="Опублікувати"
+            size="md"
+            color="gray-light"
+            className="group-hover:text-main-dark select-none"
+          />
+        </Button>
+        <Button
+          variant="clear"
+          className="group flex items-center gap-2 w-[180px] p-2.5 hover:bg-secondary-yellow rounded-lg"
+          onClick={() => rejectHandler()}
+        >
+          <Icon
+            Svg={reject}
+            width={30}
+            height={30}
+            className="stroke-disabled group-hover:stroke-main-dark"
+          />
+          <Text
+            Tag="span"
+            text="Відхилити"
+            size="md"
+            color="gray-light"
+            className="group-hover:text-main-dark select-none"
+          />
+        </Button>
+        <Button
+          variant="clear"
+          className="group flex items-center gap-2 w-[180px] p-2.5 hover:bg-secondary-yellow rounded-lg"
+          onClick={() => blockHandler()}
+        >
+          <Icon Svg={block} width={30} height={30} />
+          <Text
+            Tag="span"
+            text="Заблокувати"
+            size="md"
+            color="red"
+            className="select-none"
+          />
+        </Button>
+      </HStack>
+    </VStack>
+  );
+};
+
+export default OfferController;
