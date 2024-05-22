459e7fa4d74e966fadefd0652ecaf5a2273259a0
Reecure <69355871+Reecure@users.noreply.github.com>
2024-05-22 10:02:29 +0300

    Feature/mt 118/create managing content (#280)
    * feat: add managing banners
    
    * refactor: refactor banner managing for admin

--- frontend/src/pages/AdminPage/ui/AdminPage.tsx
+++ frontend/src/pages/AdminPage/ui/AdminPage.tsx
@@ -125,7 +125,7 @@ const AdminPage: FC = () => {
           {/* {currentTab === 1 && <ManagingSellers />} */}
           {currentTab === 2 && <ManagingOffers />}
           {/* {currentTab === 3 && <ManagingFeedbacks />} */}
-          {/* {currentTab === 4 && <ManagingContent />} */}
+          {/* {currentTab === 4 && <AdminManagingBanners />} */}
           {/* {currentTab === 5 && <Analytics />} */}
           {/* {currentTab === 6 && <Finances />} */}
           {currentTab === 7 && <SupportCenter />}
