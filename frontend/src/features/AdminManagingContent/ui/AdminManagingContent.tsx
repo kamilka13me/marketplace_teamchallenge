028a3576fa43c9b46550d6fd4948afca7227a4bb
Yurii <ogogan45@gmail.com>
2024-05-22 09:54:25 +0300

    refactor: refactor banner managing for admin

--- frontend/src/features/AdminManagingContent/ui/AdminManagingContent.tsx
+++ frontend/src/features/AdminManagingContent/ui/AdminManagingContent.tsx
@@ -1,14 +1,12 @@
 import { FC } from 'react';
 
 import AdminManagingBanners from './AdminManagingBanners';
-import AdminManagingCategories from './AdminManagingCategories';
 
 import { HStack } from '@/shared/ui/Stack';
 
 const AdminManagingContent: FC = () => {
   return (
     <HStack gap="5" className="w-full">
-      <AdminManagingCategories />
       <AdminManagingBanners />
     </HStack>
   );
