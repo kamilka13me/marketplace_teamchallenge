6b5fee670c5c271fe1b2363f6a4f287c7b8e723d
kamilka13me <kamilka13me@gmail.com>
2024-06-06 15:17:39 +0300

    feat: upd compleints

--- backend/src/components/controllers/adminController.js
+++ backend/src/components/controllers/adminController.js
@@ -6,13 +6,22 @@ import Rating from '../../models/Rate.js';
 const adminController = {
   getComplaints: async (req, res) => {
     try {
-      let { limit = 10, offset = 0 } = req.query;
+      let { limit = 10, offset = 0, sortDirection = 1, startDate, endDate } = req.query;
 
       limit = parseInt(limit, 10);
       offset = parseInt(offset, 10);
+      sortDirection = parseInt(sortDirection, 10);
 
-      // Знаходимо оскарження
-      const complaints = await Complaint.find()
+      const query = {};
+
+      if (startDate || endDate) {
+        query.createdAt = {};
+        if (startDate) query.createdAt.$gte = new Date(startDate);
+        if (endDate) query.createdAt.$lte = new Date(endDate);
+      }
+
+      const complaints = await Complaint.find(query)
+        .sort({ _id: sortDirection })
         .skip(offset)
         .limit(limit)
         .populate({
@@ -20,41 +29,56 @@ const adminController = {
           populate: [{ path: 'authorId' }, { path: 'productId', select: 'name images' }],
         });
 
-      // console.log(complaints[0].commentId);
+      const totalCount = await Complaint.countDocuments(query);
 
-      const totalCount = await Complaint.countDocuments();
-
-      // Знаходимо продукти та обчислюємо середній рейтинг
-
-      // Додаємо додаткову інформацію до оскаржень
       const complaintsWithDetails = await Promise.all(
         complaints.map(async (complaint) => {
           const comment = complaint.commentId
-            ? (await Comment.findById(complaint.commentId._id).populate({
+            ? await Comment.findById(complaint.commentId._id).populate({
                 path: 'authorId',
-                select: 'name email',
-              })) &&
-              (await Rating.findOne({
-                authorId: complaint.commentId.authorId,
-                productId: complaint.commentId.productId,
-              }).populate({
-                path: 'authorId',
-                select: 'name email',
-              }))
+                select: 'username email',
+              })
             : null;
-          // console.log(complaint);
+
+          const rating = comment
+            ? await Rating.findOne({
+                authorId: comment.authorId,
+                productId: comment.productId,
+              }).select('rating')
+            : null;
+
           const response = comment
-            ? await Comment.findOne({ parentId: comment._id }).populate('authorId')
+            ? await Comment.findOne({ parentId: complaint.commentId._id }).populate({
+                path: 'authorId',
+                select: 'username email',
+              })
             : null;
 
           const product = complaint.commentId.productId;
 
-          // const averageRating = product ? ratingsMap[product._id] || 0 : 0;
-
           return {
-            // ...complaint.toObject(),
-            comment: comment || null,
-            response: response || null,
+            _id: complaint._id,
+            reason: complaint.reason,
+            createdAt: complaint.created_at,
+            comment: comment
+              ? {
+                  _id: comment._id,
+                  username: comment.authorId.username,
+                  email: comment.authorId.email,
+                  comment: comment.comment,
+                  images: comment.images,
+                  rating: rating ? rating.rating : 0,
+                }
+              : null,
+            response: response
+              ? {
+                  _id: response._id,
+                  username: response.authorId.username,
+                  email: response.authorId.email,
+                  comment: response.comment,
+                  images: response.images,
+                }
+              : null,
             product: product
               ? { ...product.toObject() }
               : { _id: 0, name: '', price: 0, averageRating: 0 },
@@ -67,7 +91,6 @@ const adminController = {
         complaints: complaintsWithDetails,
       });
     } catch (error) {
-      // console.error(error);
       res.status(500).json({ message: 'Внутрішня помилка сервера' });
     }
   },
