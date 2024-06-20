e7bd0ecd0087360d3ea5754c92178e4258af0228
kamilka13me <kamilka13me@gmail.com>
2024-06-20 14:38:27 +0300

    fix: add token time

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -15,7 +15,7 @@ if (environment === 'development') {
       process.env.CONFIRM_SECRET_KEY ||
       '5e30285e0802c738c23dbba71efd27ab48393da8cafaf77833e1bd3f33a10c01',
     refreshTokenTime: process.env.REFRESH_TOKEN_TIME || '24h',
-    accessTokenTime: process.env.ACCESS_TOKEN_TIME || '1m',
+    accessTokenTime: process.env.ACCESS_TOKEN_TIME || '1h',
     confirmTokenTime: process.env.CONFIRM_TOKEN_TIME || '1h',
 
     mongoURL:
