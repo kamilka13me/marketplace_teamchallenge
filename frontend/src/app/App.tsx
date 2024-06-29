6af969bba2ce6ba42b614ae2e8e5ccf10fec3a55
Yurii <ogogan45@gmail.com>
2024-06-29 21:57:20 +0300

    fix: reinit user on every open site

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,5 +1,6 @@
 import { FC, Suspense, useEffect } from 'react';
 
+import Cookies from 'js-cookie';
 import { useLocation } from 'react-router-dom';
 
 import { AppRouter } from '@/app/providers/router';
@@ -11,6 +12,9 @@ import {
   userActions,
 } from '@/enteties/User';
 import { Page500 } from '@/pages/Page500';
+import { $api } from '@/shared/api/api';
+import { ApiRoutes } from '@/shared/const/apiEndpoints';
+import { COOKIE_KEY_USER } from '@/shared/const/cookies';
 import { MainLayout } from '@/shared/layouts/MainLayout';
 import MainLoaderLayout from '@/shared/layouts/MainLoaderLayout/MainLoaderLayout';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
@@ -35,6 +39,21 @@ const App: FC = () => {
       dispatch(getSellerProfileInfo({ sellerId: user._id }));
     }
   }, [dispatch, inited, user]);
+
+  useEffect(() => {
+    if (user) {
+      try {
+        $api.get(`${ApiRoutes.USER}/${user._id}`).then((res) => {
+          Cookies.set(COOKIE_KEY_USER, JSON.stringify(res.data.user));
+          dispatch(userActions.setAuthData(res.data.user));
+        });
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inited]);
 
   if (!inited) {
     return <MainLoaderLayout />;
