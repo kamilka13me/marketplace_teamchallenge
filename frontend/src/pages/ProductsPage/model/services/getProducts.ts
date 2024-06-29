6f93ad58492adb47bd3d59a37fde98c16df55980
Oleg <olegpl1993@gmail.com>
2024-06-29 23:34:04 +0300

    fix: products req in recomendation

--- frontend/src/pages/ProductsPage/model/services/getProducts.ts
+++ frontend/src/pages/ProductsPage/model/services/getProducts.ts
@@ -3,13 +3,15 @@ import { rtkApi } from '@/shared/api/rtkApi';
 export const productsApi = rtkApi.injectEndpoints({
   endpoints: (builder) => ({
     getNewProducts: builder.query({
-      query: () => `/products?limit=4&offset=0&sortDirection=-1`,
+      query: () => `/products?limit=4&offset=0&sortDirection=-1&status=published`,
     }),
     getPopularProducts: builder.query({
-      query: () => `/products?limit=4&offset=0&sortBy=views&sortDirection=-1`,
+      query: () =>
+        `/products?limit=4&offset=0&sortBy=views&sortDirection=-1&status=published`,
     }),
     getPromotionsProducts: builder.query({
-      query: () => `/products?limit=4&offset=0&sortBy=views&sortDirection=-1&discount=1`,
+      query: () =>
+        `/products?limit=4&offset=0&sortBy=views&sortDirection=-1&discount=1&status=published`,
     }),
   }),
 });
