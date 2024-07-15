6577078600f8017b0dabdcb80c9ed05cf1b48988
Oleg <olegpl1993@gmail.com>
2024-07-16 01:24:25 +0300

    feat: test login btns

--- frontend/src/features/userAuth/ui/LoginForm/LoginForm.tsx
+++ frontend/src/features/userAuth/ui/LoginForm/LoginForm.tsx
@@ -1,5 +1,6 @@
-import React, { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
+import { Switch } from '@headlessui/react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
@@ -41,6 +42,8 @@ const LoginForm: FC<LoginFormProps> = (props) => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
+  const [isTestMode, setIsTestMode] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -56,6 +59,33 @@ const LoginForm: FC<LoginFormProps> = (props) => {
       getUserByCredentials({
         email: data.inputEmail,
         password: data.inputPassword,
+      }),
+    ).then((value) => {
+      if (value.meta.requestStatus !== 'rejected') {
+        reset();
+        if (onCloseModal) {
+          onCloseModal();
+        }
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        const { role } = value.payload.user;
+
+        if (role === 'user') {
+          navigate(getRouteProfile('info'));
+        } else if (role === 'seller') {
+          navigate(getSellerProfile('dashboard'));
+        } else if (role === 'admin') {
+          navigate(getAdminProfile('users'));
+        }
+      }
+    });
+  };
+
+  const onTestSubmit = async (inputEmail: string, inputPassword: string) => {
+    await dispatch(
+      getUserByCredentials({
+        email: inputEmail,
+        password: inputPassword,
       }),
     ).then((value) => {
       if (value.meta.requestStatus !== 'rejected') {
@@ -178,6 +208,52 @@ const LoginForm: FC<LoginFormProps> = (props) => {
         disabled={!isValid}
         className="cursor-pointer outfit bg-main min-w-full py-[4px] rounded-lg font-normal text-[18px] leading-[40px] text-main-dark duration-300 hover:bg-secondary-yellow active:bg-main disabled:text-main-white disabled:bg-disabled"
       />
+      <div className="flex flex-row justify-center items-center gap-5">
+        <Switch
+          checked={isTestMode}
+          onChange={() => setIsTestMode(!isTestMode)}
+          className={`${
+            isTestMode ? 'bg-secondary-yellow' : 'bg-gray-200'
+          } relative inline-flex h-6 w-11 items-center rounded-full my-5`}
+        >
+          <span className="sr-only">Enable notifications</span>
+          <span
+            className={`${
+              isTestMode ? 'translate-x-6' : 'translate-x-1'
+            } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+          />
+        </Switch>
+        <span className="text-[16px] text-main-dark font-[600]">{t('Увійти як')}</span>
+      </div>
+
+      {isTestMode && (
+        <div className="flex flex-col gap-2">
+          <button
+            name="btnInput"
+            type="button"
+            onClick={() => onTestSubmit('olegpl1993@gmail.com', 'M987654321')}
+            className="cursor-pointer outfit bg-main min-w-full py-[4px] rounded-lg font-normal text-[18px] leading-[40px] text-main-dark duration-300 hover:bg-secondary-yellow active:bg-main"
+          >
+            Покупець
+          </button>
+          <button
+            name="btnInput"
+            type="button"
+            onClick={() => onTestSubmit('test123@gmail.com', 'M987654321')}
+            className="cursor-pointer outfit bg-main min-w-full py-[4px] rounded-lg font-normal text-[18px] leading-[40px] text-main-dark duration-300 hover:bg-secondary-yellow active:bg-main"
+          >
+            Продавець
+          </button>
+          <button
+            name="btnInput"
+            type="button"
+            onClick={() => onTestSubmit('test123@gmail.com', 'M987654321')}
+            className="cursor-pointer outfit bg-main min-w-full py-[4px] rounded-lg font-normal text-[18px] leading-[40px] text-main-dark duration-300 hover:bg-secondary-yellow active:bg-main"
+          >
+            Адміністратор
+          </button>
+        </div>
+      )}
       <VStack align="center" className="mt-6" justify="between">
         <span className="outfit text-right text-main-dark text-[14px] font-normal leading-[18px]">
           {t('Немає облікового запису?')}
