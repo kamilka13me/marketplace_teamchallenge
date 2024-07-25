e04994a7fd79630b0dd248f588982fa57f0a79e3
Oleg <olegpl1993@gmail.com>
2024-07-25 05:20:48 +0300

    feat: safari Placeholder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,18 @@
-import './index.css';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 import App from './app/App';
+import SafariPlaceholder from './app/providers/SafariPlaceholder/SafariPlaceholder';
 
 import ErrorBoundary from '@/app/providers/ErrorBoundary/ui/ErrorBoundary';
 import { StoreProvider } from '@/app/providers/StoreProvider';
 
 import '@/shared/config/i18n/i18n';
 
-import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 const container = document.getElementById('root');
 
@@ -26,7 +27,9 @@ root.render(
     <BrowserRouter>
       <StoreProvider>
         <ErrorBoundary>
-          <App />
+          <SafariPlaceholder>
+            <App />
+          </SafariPlaceholder>
         </ErrorBoundary>
       </StoreProvider>
     </BrowserRouter>
