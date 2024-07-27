4e7038ffcc380ed7c250b4cc14968fd66db63605
Oleg <olegpl1993@gmail.com>
2024-07-28 02:05:46 +0300

    fix: req for seller subscription

--- frontend/src/enteties/Seller/model/selectors/sellerInfoSelectors.ts
+++ frontend/src/enteties/Seller/model/selectors/sellerInfoSelectors.ts
@@ -1,5 +1,8 @@
 import { StateSchema } from '@/app/providers/StoreProvider';
 
 export const getSellerInfo = (state: StateSchema) => state.sellerInfo.sellerData;
+export const getSellerSubscribe = (state: StateSchema) =>
+  state.sellerInfo.sellerData?.subscribe;
+export const getSellerId = (state: StateSchema) => state.sellerInfo.sellerData?.sellerId;
 export const sellerInfoIsLoading = (state: StateSchema) => state.sellerInfo.isLoading;
 export const sellerInfoHasError = (state: StateSchema) => state.sellerInfo.error;
